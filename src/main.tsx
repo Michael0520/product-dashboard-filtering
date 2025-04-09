5772dd533b1b2f5ba8fc8c33fa1d2a14d2c3019c
Michael0520 <michael860520@gmail.com>
2025-04-10 07:00:41 +0800

    feat: add react query client provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,21 @@ import './index.css'
 // Import i18n instance
 import './lib/i18n'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>,
 ) 
